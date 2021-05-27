b386a5af1c8c81a2bb893184f5fe2138519e1597
rixiobarrios <rixiobarrios.consulado@gmail.com>
2021-05-27 04:51:44 -0500

    Accesing properties

--- README.md
+++ README.md
@@ -1,31 +1,27 @@
-**Pokemon Analyzer**
+# Pokemon Analyzer
 
-sources:
+**_Sources:_**
 
-The RESTful Pokémon API
-https://pokeapi.co/
+[The RESTful Pokémon API](https://pokeapi.co/)
 
-Axios or fetch(): Which should you use?
-https://blog.logrocket.com/axios-or-fetch-api/
+[Axios or fetch(): Which should you use?](https://blog.logrocket.com/axios-or-fetch-api/)
 
-HTTP access with promises: fetch()
-https://kth.instructure.com/courses/18551/pages/http-access-with-promises-fetch
+[HTTP access with promises: fetch()](https://kth.instructure.com/courses/18551/pages/http-access-with-promises-fetch)
 
-Arrow Functions In Javascript
-https://medium.com/@mandeepkaur1/arrow-functions-in-javascript-9254e41a80e3#:~:text=Arrow%20functions%20allow%20us%20to,to%20write%20shorter%20function%20syntax.
+[Arrow Functions In Javascript](https://medium.com/@mandeepkaur1/arrow-functions-in-javascript-9254e41a80e3#:~:text=Arrow%20functions%20allow%20us%20to,to%20write%20shorter%20function%20syntax.)
 
-Why we use then() method in JavaScript?
-https://www.geeksforgeeks.org/why-we-use-then-method-in-javascript/#:~:text=The%20then()%20method%20in,the%20code%20difficult%20to%20maintain.
+[Why we use then() method in JavaScript?](<https://www.geeksforgeeks.org/why-we-use-then-method-in-javascript/#:~:text=The%20then()%20method%20in,the%20code%20difficult%20to%20maintain.>)
 
-Axios
-https://www.npmjs.com/package/axios
+[Axios](https://www.npmjs.com/package/axios)
 
-What is the file `package.json`?
-https://nodejs.org/en/knowledge/getting-started/npm/what-is-the-file-package-json/#:~:text=All%20npm%20packages%20contain%20a,as%20handle%20the%20project's%20dependencies.
+[What is the file package.json?](https://nodejs.org/en/knowledge/getting-started/npm/what-is-the-file-package-json/#:~:text=All%20npm%20packages%20contain%20a,as%20handle%20the%20project's%20dependencies.)
 
-Node.js Introduction
-https://www.w3schools.com/nodejs/nodejs_intro.asp
+[Node.js Introduction](https://www.w3schools.com/nodejs/nodejs_intro.asp)
+
+[Property accessors](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_accessors)
+
+[Promise.prototype.finally()]
+(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/finally)
 
 Offset-based Pagination
 https://developer.box.com/guides/api-calls/pagination/offset-based/
-# stratusgrid-pokemon-analyzer
--- script.js
+++ script.js
@@ -1,59 +1,38 @@
-// console.log('This App is Working...');
-
-// // // Using axios library to make my http request from Node.js
-// const axios = require('axios'); // Importing Axios
-// const { clear } = require('node:console');
-
-// // Average will be calculated totalWeight or totalHeight divided by total Pokemons
-// const totalWeight = 0; // Setting variables with 0 values to be populated by API
-// const totalHeight = 0;
-// const totalPoke = 0;
-// let limit = 0;
-// let offset = 1;
-
-// axios
-//     .get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200.')
-//     .then(function (res) {
-//         // The then() method in JavaScript has been defined in the Promise API and is used to deal with asynchronous tasks such as an API call
-//         console.log(res.data.results);
-//     });
-
-// Set config defaults when creating the instance
-// var pokemon_api = axios.create({
-//     baseURL: 'https://pokeapi.co/api/v2/',
-// });
-
-// pokemon_api.get(`pokemon/101`).then(function (response) {
-//     console.log(
-//         `name: ${response.data.name}, height: ${response.data.height}, weight ${response.data.weight}`
-//     );
-// });
-
 const axios = require('axios'); // Importing Axios
 let totalWeight = 0; // Setting variables with 0 values to be populated by API
 let totalHeight = 0;
+let totalPoke = 0;
+
 axios
     .get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200.')
     .then((res) => {
-        let allPokemon = res.data.results;
+        let allPokemon = res.data.results; // Accessing  object properties through dot notation
         allPokemon.forEach((pokemon) => {
+            // Iterating over the array of objects using .forEach()
             axios
                 .get(pokemon.url)
                 .then((innerRes) => {
-                    const currentPokemonWeight = innerRes.data.weight;
-                    const currentPokemonHeight = innerRes.data.height;
-                    console.log(
-                        'current Pokemon weight =>',
-                        currentPokemonWeight
-                    );
-                    totalWeight += currentPokemonWeight;
-                    totalHeight += currentPokemonHeight;
+                    const pokeWeight = innerRes.data.weight;
+                    const pokeHeight = innerRes.data.height;
+                    const pokeName = innerRes.data.name;
+                    // console.log('Pokemon weight:', pokeWeight);
+                    // console.log('Pokemon height:', pokeHeight);
+                    // console.log('Pokemon total:', totalPoke);
+                    totalWeight += pokeWeight;
+                    totalHeight += pokeHeight;
+                    totalPoke++;
+                    // console.log(pokeName, pokeHeight, pokeWeight);
                 })
                 .finally(() => {
-                    const averageWeight = totalWeight / 100;
-                    const averageHeight = totalHeight / 100;
-                    console.log('Average Weight =>', averageWeight);
-                    console.log('Average Height =>', averageHeight);
+                    if ((totalPoke = allPokemon.length)) {
+                        const averageWeight = totalWeight / totalPoke;
+                        const averageHeight = totalHeight / totalPoke;
+                        // console.log('Average Weight:', averageWeight);
+                        // console.log('Average Height:', averageHeight);
+                        console.log(
+                            `totalHeight: ${totalHeight}, totalWeight: ${totalWeight}\ntotalPoke: ${totalPoke}\naverageHeight: ${averageHeight}\naverageWeight: ${averageWeight}`
+                        );
+                    }
                 });
         });
     });
